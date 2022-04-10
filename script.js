402b723c00b6000aa19b3f84704a81cddac4213a
Prabakaran R <82376928+prabakaranpkv@users.noreply.github.com>
2022-04-11 02:02:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function run() {
+  const paletteCount = document.getElementById("palette-count-input");
+  generatePalette(paletteCount.value);
+
+  [paletteCount].forEach((colorInput) => {
+    colorInput.addEventListener("input", function () {
+      generatePalette(paletteCount.value);
+    });
+  });
+}
+
+function generatePalette(paletteCount) {
+  const paletteContainer = document.getElementById("palette");
+
+  paletteContainer.innerHTML = "";
+
+  const colorsPalette = chroma
+    .scale(["#ffbaba", "#ff0000"])
+    .mode("lch")
+    .colors(paletteCount);
+  colorsPalette.forEach((palette) => {
+    const paletteItem = document.createElement("div");
+
+    paletteItem.classList.add("palette-item");
+    paletteItem.style.setProperty("--palette-color", palette);
+    paletteContainer.appendChild(paletteItem);
+  });
+}
+window.addEventListener("load", (event) => {
+  run();
+});
